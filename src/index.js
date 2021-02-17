403ab32cc10d22390b0d1dca98bdb1d180fe493a
Yunyun <celeph4is@gmail.com>
2021-02-17 14:51:38 -0800

    implement handler for deleting a person

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const Person = require('./models/person')
 const morgan = require('morgan');
 const cors = require('cors');
+const Person = require('./models/person');
 
 const app = express();
 app.use(cors());
@@ -12,7 +12,7 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :c
 
 morgan.token('content', (req, res) => JSON.stringify(req.body)); // defining :content token
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT;
 
 // connect to database
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
@@ -23,15 +23,17 @@ app.post('/api/persons', (req, res) => {
   const body = req.body;
   const { name, number } = body;
 
-  if (!name || ! number) {
+  if (!name || !number) {
     res.status(400).send('error: missing content');
   }
   const newPerson = new Person({
     name,
     number
-  })
+  });
   newPerson.save()
-    .then(data => {res.status(201).json(data)}); // res.sendStatus method sends status code, then automatically ends response (like how res.send sends the response and then ends it); res.status method only sends the status code 
+    .then(data => {
+      res.status(201).json(data)
+    }); // res.sendStatus method sends status code, then automatically ends response (like how res.send sends the response and then ends it); res.status method only sends the status code 
 });
 
 app.get('/api/persons', (req, res) => {
@@ -40,7 +42,7 @@ app.get('/api/persons', (req, res) => {
 });
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = parseInt(req.params.id);
+  const id = req.params.id;
   Person.findById(id)
     .then(data => { 
       res.json(data);
@@ -56,13 +58,12 @@ app.get('/info', (req, res) => {
     })
 });
 
-app.delete('/api/persons/:id', (req, res) => {  // not working
-  const id = parseInt(req.params.id);
+app.delete('/api/persons/:id', (req, res) => { 
+  const id = req.params.id; // this id does not need to be parsed into a number. it's mongo id object and mongo can interpret it as is
   Person.deleteOne({ _id: id }) 
     .then(() => {
       res.sendStatus(204).end();
     });
-  
 })
 
 app.listen(port, () => {
